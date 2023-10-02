8caf018aae569fd5744dc20d318fa6274ffbeb78
Namit Yadav <yadavnamit605@gmail.com>
2023-10-02 14:35:35 +0530

    ALL DONE

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('#navbar');
 
-manu.onclick = () => {
+menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 }
@@ -9,3 +9,4 @@ window.onscroll = () => {
     menu.classList.remove('bx-x');
     navbar.classList.remove('active');
 }
+
